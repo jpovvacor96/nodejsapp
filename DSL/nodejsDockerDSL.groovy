cd061876440d0af5399b3b54734977b5f400c603
USERSAD\jpalomio <jpalomio@emeal.nttdata.com>
2024-02-16 08:13:48 +0100

    Actualización del fichero con la Docker ID

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -16,7 +16,7 @@ job('Aplicacion Node.js Docker DSL') {
         dockerBuildAndPublish {
             repositoryName('jpovvacor96/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('jpovvacor96')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
